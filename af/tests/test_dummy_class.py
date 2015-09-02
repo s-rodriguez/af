946b9ced950306f1d790bf65a28406124f43745b
Sebastian Rodriguez <srodriguez@onapsis.com>
2015-09-02 16:05:09 -0300

    playing with travis

--- af/tests/test_dummy_class.py
+++ af/tests/test_dummy_class.py
@@ -7,6 +7,6 @@ class TestDummyClass(unittest.TestCase):
     def test_print_ok(self):
         number = 3
         name = 5
-        print_dummy_expected = "{number1} - {name}".format(number=number, name=name)
+        print_dummy_expected = "{number}1 - {name}".format(number=number, name=name)
         d = Foo(number, name)
         self.assertEqual(print_dummy_expected, d.print_foo(), "Different printing")
