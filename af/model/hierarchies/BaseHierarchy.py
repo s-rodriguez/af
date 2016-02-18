04492e7c0fef11ea79425ec0a7a2db915f9beaed
Sebastian Rodriguez <sebastianr213@gmail.com>
2016-02-18 20:43:24 -0300

    adding methos to base hierarchy to handle generalization levels

--- af/model/hierarchies/BaseHierarchy.py
+++ af/model/hierarchies/BaseHierarchy.py
@@ -14,10 +14,33 @@ class BaseHierarchy(object):
         return None
 
     def get_generalization_level_representation(self, starting_node, generalization_level):
-        pass
+        generalizated_node = starting_node
+        for i in range(generalization_level):
+            if generalizated_node.parent is not None:
+                generalizated_node = generalizated_node.parent
+        return generalizated_node
 
     def add_node(self, parent_node, leaf_node):
         parent_node.add_node(leaf_node)
+        self.maintain_leaf_nodes(parent_node, action='remove')
+        self.maintain_leaf_nodes(leaf_node, action='add')
+
+    def maintain_leaf_nodes(self, node, action='add'):
+        if action == 'add':
+            self.leaf_nodes.append(node)
+        elif action == 'remove':
+            if node in self.leaf_nodes:
+                self.leaf_nodes.remove(node)
+        else:
+            raise Exception('Unknown action requested')
+
+    def print_hierarchy(self, parent_node=None, level=0):
+        parent_node = self.root_node if parent_node is None else parent_node
+        print '\t'*level + str(parent_node.value)
+        next_level = level + 1
+        if parent_node.nodes is not None:
+            for node in parent_node.nodes:
+                self.print_hierarchy(node, next_level)
 
 
 class Node(object):
@@ -37,8 +60,12 @@ class Node(object):
         if self.nodes is None:
             self.nodes = []
         self.nodes.append(node)
+        node.parent = self
 
     def remove_node(self, node):
         self.nodes.remove(node)
         if len(self.nodes) == 0:
             self.nodes = None
+
+    def __repr__(self):
+        return str(self.value)
