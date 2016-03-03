ef0f047068d536a7d8c7752f20aa296e5b9c56fd
Sebastian Rodriguez <sebastianr213@gmail.com>
2016-03-03 19:36:07 -0300

    tests for node

--- af/tests/test_nodes.py
+++ af/tests/test_nodes.py
@@ -14,3 +14,17 @@ class TestNodes(unittest.TestCase):
         self.assertEqual(value, node.value, "Different node value")
         self.assertEqual(parent, node.parent, "Different node parent")
         self.assertEqual(leaf_nodes, node.nodes, "Different node leaf nodes")
+        self.assertTrue(node.is_root(), "Should be a root node")
+        self.assertFalse(node.is_leaf(), "Should not be a leaf")
+
+    def test_remove_node_ok(self):
+        value = 123
+        parent = None
+        son_node = 1
+        leaf_nodes = [son_node]
+
+        node = Node(value, parent, leaf_nodes)
+
+        self.assertTrue(len(node.nodes) == 1, "Node should have 1 son node")
+        node.remove_node(son_node)
+        self.assertTrue(node.nodes == None, "Node should no son nodes")
