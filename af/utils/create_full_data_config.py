fd75dc53d2898009ba6ca5dd92104e448acc1278
Sebastian Rodriguez <sebastianr213@gmail.com>
2016-03-19 16:44:56 -0300

    buffix on remaining call to TransformationTechnique

--- af/utils/create_full_data_config.py
+++ af/utils/create_full_data_config.py
@@ -6,7 +6,6 @@ from af.controller.hierarchies.BaseHierarchyController import BaseHierarchyContr
 
 from af.model.Attribute import Attribute
 from af.model.DataConfig import DataConfig
-from af.model.TransformationTechnique import TransformationTechnique
 from af.model.hierarchies.BaseHierarchy import BaseHierarchy
 
 import af.utils as utils
@@ -86,7 +85,7 @@ year_of_birth_hierarchy_dict = {
                                 '10/23/1965': None,
                             },
                         },
-                        '**/**/1967': {        
+                        '**/**/1967': {
                             '2/**/1967': {
                                 '2/13/1967': None,
                             },
@@ -102,33 +101,25 @@ year_of_birth_hierarchy_dict = {
 ssn_hierarchy = BaseHierarchyController(BaseHierarchy()).load_hierarchy(ssn_hierarchy_dict, attribute_type=int)
 race_hierarchy = BaseHierarchyController(BaseHierarchy()).load_hierarchy(race_hierarchy_dict, attribute_type=str)
 gender_hierarchy = BaseHierarchyController(BaseHierarchy()).load_hierarchy(gender_hierarchy_dict, attribute_type=str)
-zip_hierarchy = BaseHierarchyController(BaseHierarchy()).load_hierarchy(zip_hierarchy_dict, attribute_type=str)              
+zip_hierarchy = BaseHierarchyController(BaseHierarchy()).load_hierarchy(zip_hierarchy_dict, attribute_type=str)
 year_of_birth_hierarchy = BaseHierarchyController(BaseHierarchy()).load_hierarchy(year_of_birth_hierarchy_dict, attribute_type=str)
-
-
-# TRANSFORMATION TECHNIQUES FOR EACH QI AND IDENTIFIABLE ATTRIBUTES
-tt_ssn = TransformationTechnique('supression', ssn_hierarchy)
-tt_race = TransformationTechnique('generalization', race_hierarchy)
-tt_gender = TransformationTechnique('generalization', gender_hierarchy)
-tt_zip = TransformationTechnique('generalization', zip_hierarchy)
-tt_year_of_birth = TransformationTechnique('generalization', year_of_birth_hierarchy)
 
 
 # ATTRIBUTE INSTANCES FOR ALL ATTRIBUTES ON TABLE
 att_ssn = Attribute('ssn', basic_type='int', privacy_type=utils.PRIVACY_TYPE_IDENTIFIER)
-att_ssn.transformation_technique = tt_ssn
+att_ssn.hierarchy = ssn_hierarchy
 
 att_race = Attribute('race', privacy_type=utils.PRIVACY_TYPE_QI)
-att_race.transformation_technique = tt_race
+att_race.hierarchy = race_hierarchy
 
 att_gender = Attribute('gender', privacy_type=utils.PRIVACY_TYPE_QI)
-att_gender.transformation_technique = tt_gender
+att_gender.hierarchy = gender_hierarchy
 
 att_zip = Attribute('zip', privacy_type=utils.PRIVACY_TYPE_QI)
-att_zip.transformation_technique = tt_zip
+att_zip.hierarchy = zip_hierarchy
 
 att_year_of_birth = Attribute('year_of_birth', privacy_type=utils.PRIVACY_TYPE_QI)
-att_year_of_birth.transformation_technique = tt_year_of_birth
+att_year_of_birth.hierarchy = year_of_birth_hierarchy
 
 att_problem = Attribute('problem', privacy_type=utils.PRIVACY_TYPE_SENSITIVE)
 
